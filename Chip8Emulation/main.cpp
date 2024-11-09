588545124ed6ad61c091af75ecac26678e77494d
Huzaifah Tahir <tahirhuzaifah@gmail.com>
2024-11-09 15:24:47 -0500

    Initial Commit

---
+++ Chip8Emulation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Chip8Emulation
+//
+//  Created by Huzaifah Tahir on 11/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
